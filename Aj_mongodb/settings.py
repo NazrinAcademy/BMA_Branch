306f03f02be49ef2b082c8a842d2e4e9ea895d12
Ajeem0908 <ajiajeem2001@gmail.com>
2025-02-26 13:02:16 +0530

    modfiy the changes

--- Aj_mongodb/settings.py
+++ Aj_mongodb/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Com_app',
+    'Myapp',
     'rest_framework_simplejwt',
 ]
 
--- BMA_project/urls.py
+++ BMA_project/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for BMA_project project.
+"""BMA_Project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from Myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Com_app.urls')), 
+    path('',include('Myapp.urls')),
+    path('', include('Aj_mongodb.urls')),
 ]
